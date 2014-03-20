31b2854ac8bbfe0e99b2f2b2edd4f1cb56d07e76
cagragg2 <cagragg2@catamount.wcu.edu>
2014-03-20 02:26:45 -0400

    fixed

--- icecreamvending/settings.py
+++ icecreamvending/settings.py
@@ -51,36 +51,5 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'icecreamvending.urls'
 
-WSGI_APPLICATION = 'icecreamvending.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
 
 
