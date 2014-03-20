61f563daa0eecc7de0f86ff2a03e421b5e841169
cagragg2 <cagragg2@catamount.wcu.edu>
2014-03-20 02:33:54 -0400

    fixed

--- icecreamvending/wsgi.py
+++ icecreamvending/wsgi.py
@@ -7,9 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
+#import os
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "icecreamvending.settings")
+
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
+
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "icecreamvending.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
